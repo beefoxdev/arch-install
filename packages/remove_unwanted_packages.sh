7566043e1e15c6eea5889395707108a39282d15d
Brandon Fox <brandon.fox76@gmail.com>
2025-11-27 19:27:05 -0600

    Add script to remove unwanted packages (1password, signal, spotify)

---
+++ packages/remove_unwanted_packages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Remove unwanted packages: 1password, signal, spotify
+
+echo "Removing 1password packages..."
+sudo pacman -R --noconfirm 1password-beta 1password-cli
+
+echo "Removing signal-desktop..."
+sudo pacman -R --noconfirm signal-desktop
+
+echo "Removing spotify..."
+sudo pacman -R --noconfirm spotify
+
+echo "Package removal complete."
